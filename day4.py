8f1790330e71ad9d032d86b5f3fb68ef1ffd7872
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-08-14 15:44:02 +0530

    two pointer approach

--- day4.py
+++ day4.py
@@ -1,7 +1,8 @@
 # Day -4 
 # List : 
 #   Basics 
-#   
+#   Average or mean of a list 
+#   Separate odd/even 
 
 
 # Basics :
@@ -54,3 +55,53 @@ print(l4)
 l4.sort() 
 print(l4) 
 print("\n")
+
+
+# Average / Mean :
+def avg1(l) :
+    sum=0 
+    for x in l :
+        sum+=x 
+    n=len(l) 
+    return sum/n 
+l=[10,20,30,40,5]
+print("Average of a list :")
+print(avg1(l))
+print("\n")
+
+def avg2(l) :
+    return sum(l)/len(l)
+print("Avg of a list using sum,len functions : ")
+print(avg2(l))
+print("\n") 
+   
+
+# Separate even , odd :
+def sep1(l) :
+    even=[]
+    odd=[] 
+    for i in l :
+        if i%2==0 :
+            even.append(i)
+        else :
+            odd.append(i)    
+    return even, odd 
+l=[10,20,30,40,5]
+even,odd=sep1(l) 
+print("even : ",even)
+print("odd : ",odd)
+print("\n") 
+
+# Separating odd, even using two pointer approach : (order need not be same)
+def sep2(l) :  # odd first then even 
+    n=len(l)
+    i=-1
+    j=0
+    while j!=n :
+        if l[j]%2==1 :
+            i+=1
+            l[i],l[j]=l[j],l[i]  
+        j+=1 
+    return l 
+l=[1,3,2,8,4,3,7,8]
+print(sep2(l)) 
