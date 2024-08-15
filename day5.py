b38fdc07a17ef3fb6211fd2e6cc75eae2eb7f19f
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-08-15 11:53:43 +0530

    day5 complete

--- day5.py
+++ day5.py
@@ -1,6 +1,9 @@
 # Day - 5 
 # List 
 #   Comprehensions 
+#   Largest element in a list - 2 approaches 
+#   Second largest element in a list - 2 approaches 
+#   Check if list is sorted or not - 2 approaches  
 
 
 # Comprehension in Python : 
@@ -76,4 +79,87 @@ print("\n")
 d1={101:"gfg" , 103:"practice" , 102:"ide"} 
 d2={v:k for (v,k) in d1.items()} 
 print("Inverting a dictionary : ")
-print(d2) 
+print(d2)  
+
+## Larget element in a list : 2 approaches 
+# Approach 1 : O(n^2) time complexity 
+def max1(l) :
+    for x in l :
+        for y in l :
+            if y>x :
+                break 
+        else :
+            return x 
+    return None 
+l=[10,20,5,50]  
+print("Largest element in list : ")       
+print("Approach 1 : ",max1(l)) 
+print("\n")    
+
+# Approach 2 : O(n) time complexity 
+def max2(l) :
+    res=l[0] 
+    for i in range(1,len(l)) :
+        if l[i]>res :
+            res=l[i] 
+    return res 
+l=[10,20,3,5,50]         
+print("Largest element in list : ")       
+print("Approach 2 : ",max2(l))
+print("\n") 
+
+# Second largest element in a list :  
+def secmax1(l) :
+    if len(l)==1 :
+        return None 
+    lar=max2(l)
+    slar=None
+    for x in l :
+        if x!=lar : 
+            if slar==None :
+                slar=x 
+            else :
+                slar=max(slar,x) 
+    return slar 
+l=[10,20,40,30,100,30,90,80]                
+print("Second largest element in a list : ",secmax1(l)) 
+
+def secmax2(l) :
+    if len(l)<=1 :
+        return None 
+    lar=l[0] 
+    slar=None 
+    for x in l[1:] :
+        if x>lar :
+            slar=lar 
+            lar=x 
+        elif x!=lar :
+            if slar==None or slar<x :
+                slar=x 
+    return slar 
+l=[10,20,30,40,50,50] 
+print("Second largest in list : ",secmax2(l))
+print("\n") 
+
+## Check if list is sorted or not 
+# Approach 1 : using loop   (Preferred method)
+def sort1(l) : 
+    i=1 
+    while i<len(l) :
+        if l[i]<l[i-1] :
+            return False 
+        i+=1 
+    return True 
+l=[10,20,30,20,500]  
+print("Checking sort using loop : ",sort1(l))    
+
+# Approach 2 :  using sorted function ; sorted() - does not modify list  
+def sort2(l) :
+    sl=sorted(l) 
+    if sl==l :
+        return True 
+    else :
+        return False 
+l=[10,20,304,400]
+print("Checking sort using sp\orted function :" , sort2(l)) 
+
