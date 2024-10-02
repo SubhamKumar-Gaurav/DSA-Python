10f74a03ba28be9fe9b27e1ecd8bb67b2f001c4d
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-10-02 18:09:01 +0530

    basic recursion practice

--- day8.py
+++ day8.py
@@ -9,7 +9,6 @@ def fact(n) :
     if n==0 :
         return 1
     return n*fact(n-1) 
-
 print("Factorial of 5 using recursion : " , fact(5)) 
 
 
@@ -20,5 +19,41 @@ def fibo(n) :
     if n==1 :
         return 1 
     return fibo(n-1)+fibo(n-2) 
+print("6th term in Fibonacci series : " , fibo(6)) 
 
-print("6th term in Fibonacci series : " , fibo(6))
+## Recursion practice - I 
+def fun(n) :
+    if n==0 :
+        return 
+    print(n)
+    fun(n-1)
+    print(n) 
+print("Output for the above function : ") 
+print(fun(3))
+
+def fun(n) : 
+    if n==0 :
+        return 
+    fun(n-1) 
+    print(n) 
+    fun(n-1) 
+print("Output for above fn. : ") 
+print(fun(3))  
+
+## Recursion for practice - II 
+def fun(n) :
+    if n<=1 :
+        return 0 
+    else :
+        return 1+fun(n/2) 
+print("Output of above function : ") 
+print(fun(16))
+
+def bin(n) :
+    if n==0 :
+        return 
+    bin(n//2) 
+    print(n%2) 
+print("Binary representation of the number 13 : ")  
+print(bin(13)) 
+
