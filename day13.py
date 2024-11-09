d3b91496cf341cbabc6efcf24ae54fbd74cb5071
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-11-09 23:37:37 +0530

    Heap sort

--- day13.py
+++ day13.py
@@ -6,7 +6,7 @@
 #    Lomuto Partition 
 #    Hoare's Partition 
 #    Quick sort 
-
+#    Heap sort 
 
 ## Union of two sorted arrays 
 # Naive Approach 
@@ -213,3 +213,30 @@ def qHoareSort(arr,l,h) :
         qHoareSort(arr,l,p) 
         qHoareSort(arr,p+1,h) 
 
+
+## Heap Sort 
+# Step 1 : Build a Max Heap  
+def buildHeap(arr) : 
+    n=len(arr) 
+    for i in range((n-2)//2,-1,-1) : 
+        maxHeapify(arr,n,i) 
+
+def maxHeapify(arr,n,i) : 
+    largest=i 
+    left=2*i+1 
+    right=2*i+1 
+    if left<n and arr[left]>arr[largest] : 
+        largest=left 
+    if right<n and arr[right]>arr[largest] : 
+        largest=right 
+    if largest!=i : 
+        arr[i],arr[largest]=arr[largest],arr[i] 
+        maxHeapify(arr,n,largest) 
+
+# Step 2 : Repeatedly swap root with the last node, reduce heap size by 1 and heapify 
+def heapSort(arr) : 
+    n=len(arr) 
+    buildHeap(arr) 
+    for i in range(n-1,0,-1) : 
+        arr[i],arr[0]=arr[0],arr[i] 
+        maxHeapify(arr,i,0) 
