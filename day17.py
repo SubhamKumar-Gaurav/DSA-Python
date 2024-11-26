7d2b8b1effa50388a0cae995eadd5a74a6f33761
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-11-26 23:19:16 +0530

    Delete head of Circular Linked List

---
+++ day17.py
@@ -0,0 +1,60 @@
+#  Day - 17  
+#    Delete head of Circular Linked List 
+#    Delete Kth node of Circular Linked List 
+
+
+class Node : 
+    def __init__(self,key) :
+        self.key=key 
+        self.next=None
+ 
+def printCircular(head) : 
+    if head==None : 
+        return 
+    print(head.key, end=" ") 
+    curr=head.next 
+    while curr!=head : 
+        print(curr.key, end=" ")
+        curr=curr.next 
+
+## Delete head of Circular Linked List
+# Approach -1    (Linear Time) 
+def delHead1(head) : 
+    if head==None : 
+        return None 
+    elif head.next==head : 
+        return None 
+    curr=head 
+    while curr.next!=head : 
+        curr=curr.next 
+    curr.next=head.next 
+    return curr.next  
+
+head=Node(10)
+head.next=Node(20) 
+head.next.next=Node(30) 
+head.next.next.next=head 
+print("Delete head of Circular Linked List : ") 
+print("Approach - 1 (Linear Time)")
+printCircular(delHead1(head))
+print("\n") 
+
+
+# Approach -2  (Constant Time)  
+def delHead2(head) : 
+    if head==None : 
+        return None 
+    elif head.next==None : 
+        return None 
+    else : 
+        head.key=head.next.key 
+        head.next=head.next.next 
+        return head 
+head=Node(10)
+head.next=Node(20) 
+head.next.next=Node(30) 
+head.next.next.next=Node(40) 
+head.next.next.next.next=head 
+print("Approach - 2 (Constant Time)")
+printCircular(delHead2(head))
+print("\n") 
