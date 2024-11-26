8f9ee782811e9007e7f3ae32a747603e9fdf0a81
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-11-26 15:06:24 +0530

    Recursive approach 1 to reverse linked list

--- day15.py
+++ day15.py
@@ -197,5 +197,20 @@ head.next=Node(20)
 head.next.next=Node(30) 
 head.next.next.next=Node(40) 
 print("Reversing a List : (Efficient approach)") 
-print(reverseList2(head)) 
+print(reverseList2(head),"\n") 
 
+
+
+## Recursive reverse a Linked List (Method - 1) 
+def recursiveReverseList1(head) : 
+    ## Base case : 
+    if head==None : 
+        return head 
+    if head.next==None : 
+        return head 
+        
+    rest_head=recursiveReverseList1(head.next)
+    rest_tail=head.next 
+    rest_tail.next=head 
+    head.next=None 
+    return printList(rest_head) 
