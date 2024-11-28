6437af55470e4d7f70d64ae353829b8ba03c6777
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-11-29 00:09:17 +0530

    Reverse a DLL

--- day19.py
+++ day19.py
@@ -59,4 +59,29 @@ temp1.next=temp2
 temp2.prev=temp1 
 print("Delete Last Node of DLL : ")
 printList(delLastNode(head)) 
-print("\n") 
+print("\n")  
+
+
+## Reverse a DLL 
+def reverseDLL(head) : 
+    if head==None : 
+        return None 
+    if head.next==None : 
+        return head 
+    curr=head 
+    prev=None 
+    while curr!=None : 
+        prev=curr 
+        curr.next,curr.prev=curr.prev,curr.next 
+        curr=curr.prev 
+    return prev 
+head=Node(10) 
+temp1=Node(20) 
+temp2=Node(30) 
+head.next=temp1 
+temp1.prev=head 
+temp1.next=temp2 
+temp2.prev=temp1 
+print("Reverse a DLL : ")
+printList(reverseDLL(head)) 
+print("\n")
