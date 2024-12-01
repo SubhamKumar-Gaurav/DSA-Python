9d464830442e856db6aa633554556a9c0b71692d
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-01 23:07:54 +0530

    Check for Balanced Paranthesis

--- day20.py
+++ day20.py
@@ -61,7 +61,7 @@ class MyStack :
         self.head=self.head.next 
         self.sz=self.sz-1 
         return res 
-
+print("Linked List implementation of Stack ")
 s=MyStack() 
 s.push(10) 
 s.push(20)
@@ -69,3 +69,51 @@ s.push(30)
 print("Popped element : ",s.pop())
 print("Peak element : ",s.peak())
 print("Size of Stack : ",s.size())
+print("\n")  
+
+
+
+## Applications of Stack : 
+# Function calls 
+# Ba;
+# Balanced paranthesis
+# Reversing items 
+# Infix to postfix and prefix 
+# Evaluation of postfix/prefix 
+# Stock span problems and its variants 
+# Undo/Redo  or  Forward/Backward 
+
+
+## Check for Balanced Paranthesis 
+def isMatching(a,b) : 
+    if (a=="(" and b==")") or (a=="[" and b=="]") or (a=="{" and b=="}") :
+        return True 
+    else : 
+        return False
+
+def isBalanced(s) : 
+    stack=[] 
+    for x in s : 
+        if x in ("(", "[", "{") : 
+            stack.append(x) 
+        else : 
+            if not stack : 
+                return False 
+            elif isMatching(stack[-1],x)==False : 
+                return False 
+            else : 
+                stack.pop()
+    if stack : 
+        return False 
+    else : 
+        return True 
+print("Check for Balanced Paranthesis : ")
+s1="([])"
+s2="((())"
+s3="([)]"
+s4="{}([()])"
+print("([]) : ", isBalanced(s1))
+print("((()) : ", isBalanced(s2))
+print("([)] : ", isBalanced(s3))
+print("{}([()]) : ", isBalanced(s4))
+print("\n")
