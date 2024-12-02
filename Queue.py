3c03e409e1002196d9365f02ab40c94711cd6f1c
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-02 21:17:58 +0530

    Queue implementation using List

--- Queue.py
+++ Queue.py
@@ -1,2 +1,19 @@
 ##  Queue 
-#
+#      Queue implementation - List, collections.deque, queue.Queue 
+
+
+
+## Queue implementation using List 
+print("Queue implementation using List : ")
+q=[]
+q.append(10)
+q.append(20)
+q.append(30) 
+print("q : ",q) 
+print(q.pop(0)) 
+q.append(40) 
+print(q.pop(0))  
+print(len(q)) 
+print(q[0]) 
+print(q[-1])  
+print("\n")
