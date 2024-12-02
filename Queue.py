957ec383adfe3516668eed74d91d8bed22843dbc
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-02 22:00:35 +0530

    Linked list implementation of Queue

--- Queue.py
+++ Queue.py
@@ -1,6 +1,7 @@
 ##  Queue 
-#      Queue implementation - List, collections.deque, queue.Queue 
-
+#      Queue implementation - 
+#           * Using List, collections.deque, queue.Queue 
+#      Linked List implementation of Queue 
 
 
 ## Queue implementation using List 
@@ -34,3 +35,49 @@ print(len(q))
 print(q[0]) 
 print(q[-1]) 
 print("\n") 
+
+
+## Linked List implementation of Queue
+class Node : 
+    def __init__(self,k) : 
+        self.key=k 
+        self.next=None 
+
+class MyQueue : 
+    def __init__(self) : 
+        self.front=None 
+        self.rear=None 
+        self.sz=0 
+    def size(self) : 
+        return self.sz 
+    def isEmpty(self) : 
+        return (self.sz==0) 
+    def getFront(self) : 
+        return self.front.key 
+    def getRear(self) : 
+        return self.rear.key 
+    def enque(self, x) :   
+        temp=Node(x) 
+        if self.rear==None : 
+            self.front=temp 
+        else : 
+            self.rear.next=temp 
+        self.rear=temp 
+        self.sz=self.sz+1 
+    def deque(self) : 
+        if self.front==None : 
+            return None 
+        else : 
+            res=self.front.key 
+            self.front=self.front.next 
+            if self.front==None : 
+                self.rear=None 
+            self.sz=self.sz-1 
+            return res 
+print("Linked List implementation of Queue : ")
+q=MyQueue() 
+q.enque(10)
+q.enque(20)
+q.enque(30) 
+print(q.deque())
+print("\n")
