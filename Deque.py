5fcf7238657866b24baba259f4482b492c444f8b
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-04 00:12:48 +0530

    Program 2 and 3

--- Deque.py
+++ Deque.py
@@ -18,7 +18,21 @@ print(d)
 print("\n")
  
 
-print("Program - 2 : ") 
+print("Program - 2 : ")
+from collections import deque 
+d=deque([10,20,30,40]) 
+d.insert(2,10)    # inserting 10 at position 2 
+print("Inserting 10 at position 2 : ",d)
+print("Counting 10 : ",d.count(10)) 
+d.remove(10)
+print("Removing first occurence of 10 : ",d) 
+d.extend([50,60]) 
+print("Inserting 50 , 60 at rear : ",d) 
+d.extendleft([15,25]) 
+print("Inserting 15, 25 at front : ",d)  
+print("\n")
+
+print("Program - 3 : ") 
 from collections import deque 
 d=deque([10,20,30,40,50]) 
 d.rotate(2) 
@@ -27,3 +41,4 @@ d.rotate(-2)
 print("Rotation of 2 in anti-clockwise direction : ", d , " [gives original deque] ") 
 d.reverse() 
 print("Reversed deque : ", d ) 
+
