c30fa3d4e0e233935deb95f12ec2adce00ec07c2
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-13 21:53:20 +0530

    Binary Tree

--- Tree.py
+++ Tree.py
@@ -7,14 +7,28 @@ print("Basic terms in Tree Data Structure")
 # node, root, leaf, child, parent, subtree, descendants, ancestors, degree   
 
 
-print("Applications of Tree") 
-#  To represent hierarchical data 
-#      - Organization structure 
-#      - Folder structure 
-#      - XML/HTML Content (JSON Objects) 
-#      - In OOP (Inheritance) 
-#  Binary Search Trees 
-#  Binary Heap 
-#  B and B+ Trees in DBMS 
-#  Spanning and Shortest path tree in Computer Networks 
-#  Parse Tree, Expression Tree in Compilers 
+## Applications of Tree
+#    To represent hierarchical data 
+#       - Organization structure 
+#       - Folder structure 
+#       - XML/HTML Content (JSON Objects) 
+#       - In OOP (Inheritance) 
+#    Binary Search Trees 
+#    Binary Heap 
+#    B and B+ Trees in DBMS 
+#    Spanning and Shortest path tree in Computer Networks 
+#    Parse Tree, Expression Tree in Compilers 
+
+
+## Binary Tree in Python
+class Node : 
+    def __init__(self, k): 
+        self.left=None 
+        self.right=None 
+        self.key=k 
+
+# Driver code : 
+root=Node(10) 
+root.left=Node(20)
+root.right=Node(20)
+root.left.left=Node(40) 
