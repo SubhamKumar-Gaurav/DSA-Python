abc617ac883565023a844868f5ab494efa841aec
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-13 21:32:03 +0530

    Basic Terms in Tree

---
+++ Tree.py
@@ -0,0 +1,19 @@
+##  Tree 
+#     Basic terms 
+
+
+
+print("Basic terms in Tree Data Structure")  
+# node, root, leaf, child, parent, subtree, descendants, ancestors, degree   
+
+## Applications of Tree 
+#  To represent hierarchical data 
+#      - Organization structure 
+#      - Folder structure 
+#      - XML/HTML Content (JSON Objects) 
+#      - In OOP (Inheritance) 
+#  Binary Search Trees 
+#  Binary Heap 
+#  B and B+ Trees in DBMS 
+#  Spanning and Shortest path tree in Computer Networks 
+#  Parse Tree, Expression Tree in Compilers 
