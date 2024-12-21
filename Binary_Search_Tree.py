671ac94c149ecfb01501458fc6dc97b34452b51e
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-21 19:03:39 +0530

    Insert in BST - Recursive

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -20,6 +20,7 @@ class Node :
         self.right=None 
         self.key=k 
 
+
 ## Search in BST  
 # Recursive implementation 
 print("Search in BST - Recursive approach ")
@@ -66,4 +67,42 @@ root.right.right.left=Node(50)
 root.right.right.right=Node(100) 
 print("100 : ", searchBST2(root, 100))
 print("15 : ", searchBST2(root, 15)) 
-print("\n") 
+print("\n")  
+
+def printKDist(root,k) : 
+    if root is None : 
+        return 
+    if k==0 : 
+        print(root.key, end=" ")
+    else : 
+        printKDist(root.left, k-1)
+        printKDist(root.right, k-1)
+
+def height(root) : 
+    if root==None : 
+        return 0 
+    else : 
+        return max(height(root.left),height(root.right))+1 
+
+## Insert in BST  
+# Recursive approach 
+def insert1(root, key) : 
+    if root==None : 
+        return Node(key) 
+    elif root.key==key : 
+        return root 
+    elif root.key>key : 
+        root.left=insert1(root.left, key) 
+    else : 
+        root.right=insert1(root.right, key) 
+    return root 
+root=None 
+root=insert1(root, 10)
+root=insert1(root, 20)
+root=insert1(root, 5) 
+print("Insert in BST (Recursive approach) : ") 
+
+h=height(root) 
+for k in range(h) : 
+    printKDist(root,k) 
+print("\n")
