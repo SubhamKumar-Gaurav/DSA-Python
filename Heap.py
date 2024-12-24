f621540aa1a65edf0440527f981057d55f185279
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-25 01:46:05 +0530

    heapq ops

--- Heap.py
+++ Heap.py
@@ -133,4 +133,20 @@ def heapSort(arr) :
     buildHeap(arr) 
     for i in range(n-1,0,-1) : 
         arr[i], arr[0] = arr[0], arr[i] 
-        maxHeapify(arr, i, 0)
+        maxHeapify(arr, i, 0) 
+
+
+
+## heapq 
+import heapq 
+print("heapq : ")
+pq=[5,20,1,30,4] 
+heapq.heapify(pq) 
+print("heapify : ",pq) 
+
+heapq.heappush(pq,3) 
+print("heapq.heappush(pq,3) : ",pq) 
+
+print("heapq.heappop(pq) : ",heapq.heappop(pq))
+print(pq) 
+print("\n") 
