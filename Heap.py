5532a3415557058d87e4d4207a76154dc437fd7b
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-25 01:30:52 +0530

    HeapSort - step 2

--- Heap.py
+++ Heap.py
@@ -127,3 +127,10 @@ def maxHeapify(arr, n, i) :
     if largest!=i : 
         arr[i], arr[largest] = arr[largest], arr[i] 
         maxHeapify(arr, n , largest) 
+
+def heapSort(arr) : 
+    n=len(arr) 
+    buildHeap(arr) 
+    for i in range(n-1,0,-1) : 
+        arr[i], arr[0] = arr[0], arr[i] 
+        maxHeapify(arr, i, 0)
