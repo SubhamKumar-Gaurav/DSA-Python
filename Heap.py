996f30f14a7b0155a983993dc33a32859fe90d9b
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-24 21:43:56 +0530

    Heap - Intro

---
+++ Heap.py
@@ -0,0 +1,23 @@
+## Heap 
+#    Introduction  
+#    Heap Implementation  
+#    Binary Heap Insert
+#    Binary Heap (Extract min and Heapify) 
+#    Decrease Key and Delete operations 
+#    Build Heap 
+#    Heap sort 
+#    Heapq in Python 
+
+
+
+
+## Introduction  
+#    Used in Heap Sort 
+#    Used to implement Priority Queue 
+#    Two types  (Min Heap and Max Heap) 
+
+#   Binary Heap is a complete Binary tree (Stored as an array)   
+
+
+## Heap implementation 
+print("Heap implementation") 
