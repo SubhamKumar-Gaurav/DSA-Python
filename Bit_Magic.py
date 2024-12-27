bc931d7742fe49068595b3b27d6c0eff310f95dc
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-28 00:22:31 +0530

    Bit Manipulation

---
+++ Bit_Magic.py
@@ -0,0 +1,10 @@
+## Bit Magic 
+#   Bitwise operators 
+#   Binary representation of negative number 
+#   Check Kth bit is set or not 
+#   Count set bits 
+#   Find the only odd 
+#   Power of 2
+#   One Odd occuring 
+#   Power set using Bitwise 
+
