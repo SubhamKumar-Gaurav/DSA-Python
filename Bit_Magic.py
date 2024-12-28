2d2590d1dad79c10181f8dfc3a8843e93d7b19de
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-28 21:52:25 +0530

    Bitwise AND

--- Bit_Magic.py
+++ Bit_Magic.py
@@ -13,5 +13,9 @@
 ## Bitwise operators  
 print("binary of 18 : ", bin(18)) 
 print("binary of 12 : ", bin(12)) 
-print("decimal of 0b10010 : ", int("0b10010",2))
-print("decimal of 0b1100 : ", int("0b1100",2))
+print("decimal of 0b10010 : ", int("0b10010",2)) 
+print("decimal of 0b1100 : ", int("0b1100",2))   
+
+x=3 
+y=6 
+print("bitwise AND of 3 and 6 : ", x&y)
