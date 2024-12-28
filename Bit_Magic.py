6f654cb57d01f2173a89b6d878550f11c50c74a1
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-28 22:24:17 +0530

    Left shift operator

--- Bit_Magic.py
+++ Bit_Magic.py
@@ -19,7 +19,12 @@ print("decimal of 0b1100 : ", int("0b1100",2))
 x=3 
 y=6 
 print("bitwise AND (&) of 3 and 6 : ", x&y)
-
 print("bitwise OR (|) of 3 and 6 : ", 3|6) 
-
 print("bitwise XOR (^) of 3 and 6 : ", 3^6) 
+print("\n") 
+
+x=5 
+print("Left shift operator ") 
+print(x<<1)  # Multiplying with 2^1 
+print(x<<2)  # Multiplying with 2^2 
+print(x<<10)  # Multiplying with 2^10  
