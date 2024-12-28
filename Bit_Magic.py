0855822f676199b085e1310b3af6c202c0b86dcb
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-28 22:31:09 +0530

    Right shift operator

--- Bit_Magic.py
+++ Bit_Magic.py
@@ -27,4 +27,11 @@ x=5
 print("Left shift operator ") 
 print(x<<1)  # Multiplying with 2^1 
 print(x<<2)  # Multiplying with 2^2 
-print(x<<10)  # Multiplying with 2^10  
+print(x<<3)  # Multiplying with 2^3  
+print("\n") 
+
+x=50 
+print("Right shift operator ") 
+print(x>>1)  # Floor division with 2^1  
+print(x>>2)  # Floor division with 2^2  
+print(x>>3)  # Floor division with 2^3  
