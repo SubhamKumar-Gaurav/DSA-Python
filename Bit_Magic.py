49b042f62582f292ba6642ca57f10db9c4b33387
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-28 21:46:06 +0530

    bin and int

--- Bit_Magic.py
+++ Bit_Magic.py
@@ -8,3 +8,10 @@
 #   One Odd occuring 
 #   Power set using Bitwise 
 
+
+
+## Bitwise operators  
+print("binary of 18 : ", bin(18)) 
+print("binary of 12 : ", bin(12)) 
+print("decimal of 0b10010 : ", int("0b10010",2))
+print("decimal of 0b1100 : ", int("0b1100",2))
