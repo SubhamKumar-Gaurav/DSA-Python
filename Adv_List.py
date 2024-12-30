0b1e0368c695bc4df975766f514065be6d505baf
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-30 22:56:02 +0530

    Left rotate: Using deque

--- Adv_List.py
+++ Adv_List.py
@@ -11,3 +11,14 @@ l=[10,20,30,40,50]
 d=2 
 l=l[d:]+l[:d] 
 print(l) 
+print("\n") 
+
+print("Using deque : ")
+from collections import deque
+l=[10,20,30,40,50]
+d=2 
+dq=deque(l) 
+dq.rotate(-d) 
+l=list(dq) 
+print(l) 
+print("\n") 
