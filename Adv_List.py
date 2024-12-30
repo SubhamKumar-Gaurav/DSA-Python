c1aa7a95a5d3e725735ad998b34c98f08ba6b337
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-30 22:54:04 +0530

    Left rotate: String slicing

---
+++ Adv_List.py
@@ -0,0 +1,13 @@
+## Advanced List  
+#   Left rotate by d places 
+
+
+
+
+## Left rotate by d places 
+print("Left rotate by d places ")
+print("Using String Slicing : ")
+l=[10,20,30,40,50]
+d=2 
+l=l[d:]+l[:d] 
+print(l) 
