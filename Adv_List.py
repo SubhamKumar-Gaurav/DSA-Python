ff600913a1433b50826ff94123df90077bdcf089
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2024-12-30 23:03:22 +0530

    Left rotate: pop and append

--- Adv_List.py
+++ Adv_List.py
@@ -6,13 +6,15 @@
 
 ## Left rotate by d places 
 print("Left rotate by d places ")
-print("Using String Slicing : ")
+# Using List slicing
+print("Using List Slicing : ")
 l=[10,20,30,40,50]
 d=2 
 l=l[d:]+l[:d] 
 print(l) 
 print("\n") 
 
+# Using deque
 print("Using deque : ")
 from collections import deque
 l=[10,20,30,40,50]
@@ -21,4 +23,15 @@ dq=deque(l)
 dq.rotate(-d) 
 l=list(dq) 
 print(l) 
+print("\n") 
+
+# Using pop and append 
+def leftRotate(l,d) : 
+    for i in range(d) : 
+        l.append(l.pop(0))
+    print(l)
+l=[10,20,30,40,50]
+d=3 
+print("Using pop and append")
+leftRotate(l,d) 
 print("\n") 
