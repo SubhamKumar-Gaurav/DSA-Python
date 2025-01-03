c960328c7aef042dd439b0f084133690e737dffa
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-04 01:51:45 +0530

    Maximum Circular Sum Subarray: Naive

--- Adv_List.py
+++ Adv_List.py
@@ -238,3 +238,19 @@ print("\n")
 
 
 ## Maximum Circular Sum Subarray 
+print("Maximum Circular Sum Subarray: Naive")
+def maxCircularSum(arr) : 
+    n=len(arr)
+    res=arr[0] 
+    for i in range(n) : 
+        curr_max=arr[i]
+        curr_sum=arr[i]
+        for j in range(1,n) : 
+            index=(i+j)%n
+            curr_sum+=arr[index]
+            curr_max=max(curr_max, curr_sum) 
+        res=max(res, curr_max) 
+    return res 
+arr=[5,-2,3,4] 
+print("[5,-2,3,4] : ",maxCircularSum(arr)) 
+print("\n") 
