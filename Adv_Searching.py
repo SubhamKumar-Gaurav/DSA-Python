1eae0aebc29f94685a0015b3d8f8fd292c83e4e4
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-11 12:36:45 +0530

    Search in sorted rotated array

--- Adv_Searching.py
+++ Adv_Searching.py
@@ -1,6 +1,6 @@
 ## Advanced searching 
 #    Search in an Infinite sized array 
-#    
+#    Search in sorted rotated array 
 
 
 ## Search in an Infinite sized array  
@@ -39,4 +39,33 @@ def searchInfy2(arr,x) :
 arr=[1,10,15,20,40,80,90,100,120,500,600,700,1000]
 x=100 
 print("Search in infinite arr (Efficient) : ",searchInfy2(arr,x)) 
+print("\n") 
+
+
+## Search in sorted rotated array 
+# 1. Naive approach: Linear Search 
+
+# 2. Efficient approach 
+def searchSortRot(arr,x) : 
+    n=len(arr)  
+    low=0 
+    high=n-1  
+    while low<=high : 
+        mid=(low+high)//2 
+        if arr[mid]==x : 
+            return mid 
+        if arr[low]<arr[mid] : 
+            if arr[low] <= x < arr[mid] : 
+                high=mid-1 
+            else : 
+                low=mid+1 
+        else : 
+            if arr[mid] < x <= arr[high] : 
+                low=mid+1 
+            else : 
+                high=mid-1 
+    return -1 
+arr=[10,20,30,40,50,8,9] 
+x=30 
+print("Search in sorted rotated array : ",searchSortRot(arr,x))
 print("\n") 
