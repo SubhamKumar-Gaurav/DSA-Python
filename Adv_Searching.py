dbf63849b250d173a1b6e1cc9487d96018dd98d2
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-11 12:09:28 +0530

    Search in infinite arr (Efficient)

--- Adv_Searching.py
+++ Adv_Searching.py
@@ -5,7 +5,7 @@
 
 ## Search in an Infinite sized array  
 ## 1. Naive Approach 
-def searchInfy(arr,x) :
+def searchInfy1(arr,x) :
     i=0 
     while True : 
         if arr[i]==x : 
@@ -13,3 +13,30 @@ def searchInfy(arr,x) :
         elif arr[i]>x : 
             return -1 
         i+=1 
+
+
+## 2. Efficient approach 
+def bSearch(arr,l,r,x) : 
+    while l<=r : 
+        mid=(l+r)//2 
+        if arr[mid]==x : 
+            return mid 
+        elif arr[mid]<x : 
+            l=mid+1 
+        else : 
+            r=mid-1 
+    return -1 
+
+def searchInfy2(arr,x) : 
+    if arr[0]==x : 
+        return 0 
+    i=1 
+    while arr[i]<x : 
+        i=i*2 
+    if arr[i]==x : 
+        return i 
+    return bSearch(arr,i//2,i-1,x) 
+arr=[1,10,15,20,40,80,90,100,120,500,600,700,1000]
+x=100 
+print("Search in infinite arr (Efficient) : ",searchInfy2(arr,x)) 
+print("\n") 
