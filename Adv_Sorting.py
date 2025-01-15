c6e7b03452938f403f6697de3cee70b5e214bce9
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-15 15:02:51 +0530

    Tail call elimination in qSort

--- Adv_Sorting.py
+++ Adv_Sorting.py
@@ -16,3 +16,16 @@
 #    Bucket sort - Python implementation  
 
 
+## Tail call elimination in Quick sort
+def qSort(arr,l,r) : 
+    if l<r : 
+        p=partition(arr,l,r)
+        qSort(arr,l,b)
+        qSort(arr,p+1,r) 
+
+# Rewritten code (Tail call optimization)
+def qSort(arr,l,r) : 
+    while l<r : 
+        p=partition(arr,l,r) 
+        qSort(arr,l,p) 
+        l=p+1 
