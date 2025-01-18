6f9e4c8261d151e95cabdeaff75a52f1e82290a7
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-18 09:28:30 +0530

    Segregate 0, 1 and 2: Efficient

--- Adv_Sorting.py
+++ Adv_Sorting.py
@@ -122,7 +122,6 @@ def posNeg(arr) :
 
 
 
-
 ## Sort an array with three types of element 
 def sort3Types(arr) :  
     temp=[]
@@ -138,3 +137,23 @@ def sort3Types(arr) :
     for i in range(len(arr)) : 
         arr[i]=temp[i] 
     return arr 
+
+
+# Efficient approach 
+def segregate3(arr) : 
+    low=0 
+    mid=0 
+    high=len(arr)-1 
+    while mid<=high : 
+        if arr[mid]==0 : 
+            arr[low], arr[mid] = arr[mid], arr[low] 
+            low+=1 
+            mid+=1 
+        elif arr[mid]==1 : 
+            mid+=1 
+        else : 
+            arr[high], arr[mid] = arr[mid], arr[high] 
+            high-=1 
+    return arr 
+arr=[1,2,1,1,2,0] 
+print(segregate3(arr))
