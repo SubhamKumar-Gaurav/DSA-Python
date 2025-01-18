ed40dd3564efe226b662ed72b84e234c89c1da1a
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-18 11:14:37 +0530

    Meeting max Guests

--- Adv_Sorting.py
+++ Adv_Sorting.py
@@ -170,3 +170,27 @@ def mergeIntervals(arr) :
             arr[res]=arr[i]
     for i in range(res+1) : 
         print(arr[i], end=" ")   
+ 
+
+## Meeting the maximum guests  
+def maxGuests(arr,dept) : 
+    n=len(arr)
+    arr.sort() 
+    dept.sort() 
+    res=1
+    curr=1  
+    i=1 
+    j=0 
+    while i<n and j<n : 
+        if arr[i]<=dept[j] : 
+            curr+=1 
+            i+=1 
+        else : 
+            curr-=1 
+            j+=1 
+        res=max(res,curr) 
+    return res 
+
+arr=[900,940,950,1100,1500,1800]
+dept=[910,1200,1120,1130,1900,2000]
+print(maxGuests(arr,dept))  
