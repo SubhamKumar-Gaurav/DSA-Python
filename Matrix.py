087e7cf0250d136054fbfde4b76351884e600b87
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-24 03:30:41 +0530

    Transpose of a matrix

--- Matrix.py
+++ Matrix.py
@@ -101,3 +101,15 @@ def boundary(mat) :
 mat=[[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]] 
 boundary(mat)
 print("\n") 
+
+
+## Transpose of a Matrix 
+print("Transpose of a Matrix")  
+def transpose(mat) : 
+    n=len(mat) 
+    for i in range(n) : 
+        for j in range(i+1,n) : 
+            mat[i][j] , mat[j][i] = mat[j][i], mat[i][j] 
+    return mat  
+mat=[[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]] 
+print(transpose(mat))
