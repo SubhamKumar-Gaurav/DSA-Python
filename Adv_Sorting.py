d4d09a59fbe8e62038a4b6574ede4084bb937112
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-23 23:23:51 +0530

    Radix sort

--- Adv_Sorting.py
+++ Adv_Sorting.py
@@ -249,3 +249,28 @@ def cycleSort(arr) :
             arr[pos], item = item, arr[pos]
     return arr 
 
+
+## Radix sort 
+def radixSort(arr) : 
+    mx=max(arr) 
+    exp=1 
+    while mx//exp>0 : 
+        countingSort(arr,exp) 
+        exp*=10 
+    return arr 
+def countingSort(arr,exp) : 
+    output=[0]*len(arr) 
+    count=[0]*10 
+    for i in range(len(arr)) : 
+        index=(arr[i]//exp)%10 
+        count[index]+=1 
+    for i in range(1,10) : 
+        count[i]+=count[i-1] 
+    i=len(arr)-1 
+    while i>=0 : 
+        index=(arr[i]//exp)%10 
+        output[count[index]-1]=arr[i] 
+        count[index]-=1 
+        i-=1 
+    for i in range(len(arr)) : 
+        arr[i]=output[i] 
