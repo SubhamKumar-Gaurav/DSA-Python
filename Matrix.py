4d70ab5fc0b6db24d1cd098574ca939406e9b042
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-24 02:00:05 +0530

    User specified dimensions

--- Matrix.py
+++ Matrix.py
@@ -29,3 +29,20 @@ for i in range(len(arr)) :
     for j in range(len(arr[i])) : 
         print(arr[i][j], end=" ")
     print() 
+
+# User specified dimensions - Not recommended 
+print("User specified dimensions - Not recommended ")
+rows=3 
+cols=4 
+arr=[[0]*cols]*rows 
+arr[0][0]=1 
+for r in arr : 
+    print(r)
+print() 
+
+print("Recommended way ")
+arr=[[0 for i in range(cols)] for j in range(rows)]
+arr[0][0]=1 
+for r in arr : 
+    print(r) 
+print() 
