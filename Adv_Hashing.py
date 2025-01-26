c5291ada500c4b27b4570e8b2adb88774003dbc4
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-26 12:15:49 +0530

    updatd index

---
+++ Adv_Hashing.py
@@ -0,0 +1,14 @@
+## Adv Hashing 
+#   Intersection of two arrays 
+#   Union of two unsorted array 
+#   Pair with given sum in unsorted array 
+#   Subarray with 0 sum  
+#   Subarray with given sum 
+#   Check for Palindrome Permutation
+#   Longest Subarray with given sum 
+#   Longest subarray with equal number of zero and one 
+#   Longest common span with same sum in Binary array 
+#   Longest consecutive subsequence 
+#   Count distinct elements in every window 
+#   More than n/k occurence 
+#   More than n/k occurences  [ O(nk) solution ]
