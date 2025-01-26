2c07cd8b1f4907fc84c1f1465897c46a614f51a3
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-26 12:40:37 +0530

    Intersectionof two arrays

--- Adv_Hashing.py
+++ Adv_Hashing.py
@@ -12,3 +12,23 @@
 #   Count distinct elements in every window 
 #   More than n/k occurence 
 #   More than n/k occurences  [ O(nk) solution ]
+
+
+## Intersection of two arrays  
+# Naive approach 
+def intersection_a_b(arr1,arr2) : 
+    m=len(arr1)
+    n=len(arr2) 
+    res=0 
+    for i in range(m) : 
+        flag=False 
+        for j in range(i) : 
+            if arr1[i]==arr1[j] : 
+                flag=True 
+        if flag==True : 
+            continue 
+        for j in range(n) : 
+            if arr1[i]==arr2[j] : 
+                res+=1 
+                break 
+    return res 
