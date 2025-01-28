8252faf29c79a1c937aafaaf182af6fb7c87cf06
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-28 22:31:06 +0530

    Longest common span: Naive

--- Adv_Hashing.py
+++ Adv_Hashing.py
@@ -230,3 +230,19 @@ def longest01Sub(arr) :
         else : 
             mydict[x]=i 
     return res 
+
+
+## Longest common span with same sum in Binary array  
+# Naive approach 
+def longestCommonSpan(arr1, arr2) : 
+    n=len(arr1)
+    res=0 
+    for i in range(n) : 
+        sum1=0 
+        sum2=0 
+        for j in range(i,n) : 
+            sum1+=arr1[j]
+            sum2+=arr2[j] 
+            if sum1==sum2 : 
+                res=max(res, j-i+1) 
+    return res 
