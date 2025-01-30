c4d0b33b3d8ea79fbe6d2d32dcb0724203adfb12
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-01-31 01:05:44 +0530

    Pattern Search: Naive

--- Adv_String.py
+++ Adv_String.py
@@ -10,3 +10,16 @@ def patternSearching(txt, pat) :
     while pos>=0 : 
         print(pos)
         pos=txt.find(pat) 
+
+# Naive approach 
+def naivePat(txt, pat) : 
+    n=len(txt) 
+    m=len(pat) 
+    for i in range(n-m+1) : 
+        j=0 
+        while j<m : 
+            if pat[j]!=txt[i+j] : 
+                break 
+            j+=1 
+        if j==m : 
+            print(i, end=" ")
