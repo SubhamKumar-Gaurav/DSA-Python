88d09109919fb3f0fdbf62d028d0bfb931eb1c0e
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-02-02 16:49:37 +0530

    Updated index

---
+++ Adv_Linked_List.py
@@ -0,0 +1,13 @@
+## Advanced Linked List 
+#   Reverse a linked list in group of size k 
+#   Detect loop using floyd's cycle detection algorithm 
+#   Detect and remove loop in linked list 
+#   Intersection point of two linked list 
+#   Segregate Even and Odd nodes 
+#   Pairwise swap nodes 
+#   Clone a linked list with random pointer 
+#   Merge two sorted linked lists 
+#   Palindrome Linked List 
+
+
+## Reverse a linked list in group of size k  
