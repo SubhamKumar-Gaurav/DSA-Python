65ad25adb543521caf6bfd3dea94a09a9bb36ee7
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-02-02 21:45:11 +0530

    Reverse LL in grp of k: Recursive

--- Adv_Linked_List.py
+++ Adv_Linked_List.py
@@ -10,4 +10,44 @@
 #   Palindrome Linked List 
 
 
-## Reverse a linked list in group of size k  
+# LL class 
+class Node : 
+    def __init__(self, k) : 
+        self.key=k 
+        self.next=None
+
+# print list 
+def printList(head) : 
+    curr=head 
+    while curr!=None : 
+        print(curr.key) 
+        curr=curr.next 
+
+## Reverse a linked list in group of size k 
+# Recursive solution  
+def recRevK(head, k) : 
+    curr=head 
+    prev , next = None, None 
+    count=0 
+    while curr!=None and count<k : 
+        next=curr.next 
+        curr.next=prev 
+        prev=curr 
+        curr=next 
+        count+=1 
+    if next!=None : 
+        rem_head=recRevK(curr, k) 
+        head.next=rem_head
+    return prev 
+
+print("Reverse LL in grp of k: Recursive")
+head = Node(10)
+head.next = Node(20)
+head.next.next = Node(30)
+head.next.next.next = Node(40)
+head.next.next.next.next = Node(50)
+head.next.next.next.next.next = Node(60)
+head.next.next.next.next.next.next = Node(70)
+head=recRevK(head, 3)
+printList(head) 
+print() 
