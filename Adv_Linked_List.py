eadcc4c837d2797fbf400c57c4a9de6f0415aac0
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-02-05 00:47:58 +0530

    Merge 2 sorted LL

--- Adv_Linked_List.py
+++ Adv_Linked_List.py
@@ -296,3 +296,32 @@ def clone(h1) :
         clone=clone.next 
         curr=curr.next 
     return h2 
+
+## Merge two sorted linked lists 
+def sortedMerge(a, b) : 
+    if a==None : 
+        return b 
+    if b==None : 
+        return a 
+    head, tail = None, None 
+    if a.key<=b.key : 
+        head=tail=a 
+        a=a.next 
+    else : 
+        head=tail=b 
+        b=b.next 
+    while a!=None and b!=None : 
+        if a.key<=b.key : 
+            tail.next=a 
+            tail=a 
+            tail=a 
+            a=a.next 
+        else : 
+            tail.next = b 
+            tail = b 
+            b = b.next 
+    if a==None : 
+        tail.next = b 
+    else : 
+        tail.next = a 
+    return head 
