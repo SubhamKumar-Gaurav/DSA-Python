a6f48ddd37e3fa7509b807cad151f90122ac681a
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-02-17 11:14:56 +0530

    Evaluation of Prefix

--- Adv_Stack.py
+++ Adv_Stack.py
@@ -576,3 +576,27 @@ print( infixToPrefix(s))
 print("\n")
 
 
+## Evaluation of Prefix 
+def is_operand(c):
+	return c.isdigit()
+
+def evaluate(expression):
+	stack = []
+	for c in expression[::-1]:
+		if is_operand(c):
+			stack.append(int(c))
+		else:
+			o1 = stack.pop()
+			o2 = stack.pop()
+			if c == '+':
+				stack.append(o1 + o2)
+			elif c == '-':
+				stack.append(o1 - o2)
+			elif c == '*':
+				stack.append(o1 * o2)
+			elif c == '/':
+				stack.append(o1 / o2)
+	return stack.pop()
+print("Evaluation of Prefix: ") 
+test_expression =["+", "9", "*", "2", "6"] 
+print(evaluate(test_expression)) 
