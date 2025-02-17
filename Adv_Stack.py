fde2c4eca7977d79465accbbcdadcaf1ed241d6b
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-02-17 11:12:28 +0530

    Infix to Prefix

--- Adv_Stack.py
+++ Adv_Stack.py
@@ -518,3 +518,61 @@ exp=['10', '2', '*', '3', '+']
 obj=Evaluate1(len(exp)) 
 print(obj.evaluatePostfix(exp)) 
 print("\n") 
+
+
+
+## Infix to Prefix (Efficient solution)
+print("Infix to Prefix: Efficient solution") 
+def isOperator(c):
+	return not c.isalnum() 
+
+def getPriority(C):
+	if (C == '-' or C == '+'):
+		return 1
+	elif (C == '*' or C == '/'):
+		return 2
+	elif (C == '^'):
+		return 3
+	return 0
+
+def infixToPrefix(infix):
+	operators = []
+	operands = []
+	for i in range(len(infix)):
+		if (infix[i] == '('):
+			operators.append(infix[i])
+		elif (infix[i] == ')'):
+			while (len(operators)!=0 and operators[-1] != '('):
+				op1 = operands.pop()
+				op2 = operands.pop()
+				op = operators.pop()				
+				tmp = op + op2 + op1
+				operands.append(tmp)
+			operators.pop()
+		elif (not isOperator(infix[i])):
+			operands.append(infix[i] + "")
+		else:
+			while (len(operators)!=0 and getPriority(infix[i]) <= getPriority(operators[-1])):
+				op1 = operands.pop()
+				op2 = operands.pop()
+				op =  operators.pop()
+				tmp = op + op2 + op1
+				operands.append(tmp)
+			operators.append(infix[i])
+
+	while (len(operators)!=0):
+		op1 = operands.pop()		
+		op2 = operands.pop()		
+		op = operators.pop()		
+		tmp = op + op2 + op1
+		operands.append(tmp)
+	return operands[-1]
+
+s = "(A-B/C)*(A/K-L)"
+print( infixToPrefix(s))
+  
+s = "x+y*z/w+u"
+print( infixToPrefix(s))
+print("\n")
+
+
