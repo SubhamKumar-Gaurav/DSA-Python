c5772a10984508db209792df258c56fbe8bba069
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-03-01 22:41:50 +0530

    Advanced Tree

---
+++ Adv_Tree.py
@@ -0,0 +1,21 @@
+# Adv Tree 
+#   Level Order Traversal by Line - 2 codes 
+#   Check for Balanced Binary Tree 
+#   Vertical Traversal of Binary tree 
+#   Bottom View of Binary Tree 
+#   Maximum Width of Binary Tree 
+#   Convert Binary Tree to Doubly Linked List 
+#   Construct Binary Tree from Inorder and Preorder 
+#   Tree Traversal in Spiral Form 
+#   Diameter of a Binary Tree 
+#   LCA of Binary Tree 
+#   Burn a Binary Tree from a Leaf 
+#   Count Nodes in a Complete Binary Tree 
+#   Serialize and Deserialize a Binary Tree 
+#   Iterative Inorder Traversal 
+#   Iterative Preorder Traversal 
+#   Iterative Preorder Traversal (Space optimized) 
+
+
+## Level Order Traversal by Line
+# Code - 1 
