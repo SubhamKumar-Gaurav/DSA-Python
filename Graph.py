3940eba993b17146234d59d0a17af3fae2f06315
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-03-26 05:36:17 +0530

    Articulation points

--- Graph.py
+++ Graph.py
@@ -591,3 +591,144 @@ BellmanFord(adj, 0)
 print("\n") 
 
 
+
+## Articulation Point 
+# Python program to find articulation points in an undirected graph
+
+from collections import defaultdict
+class Graph:
+	def __init__(self, vertices):
+		self.V = vertices 
+		self.graph = defaultdict(list) 
+		self.Time = 0
+	
+	def addEdge(self, u, v):        # function to add an edge to graph
+		self.graph[u].append(v)
+		self.graph[v].append(u)
+
+	def APUtil(self, u, visited, ap, parent, low, disc):     # Count of children in current node		
+		children = 0
+		visited[u]= True    # Mark the current node as visited and print it
+		disc[u] = self.Time    # Initialize discovery time 
+		low[u] = self.Time     # Initialize low value
+		self.Time += 1
+
+		for v in self.graph[u]:     # Recur for all the vertices adjacent to this vertex
+			# If v is not visited yet, then make it a child of u
+			# in DFS tree and recur for it
+			if visited[v] == False :
+				parent[v] = u
+				children += 1
+				self.APUtil(v, visited, ap, parent, low, disc)
+
+				# Check if the subtree rooted with v has a connection to
+				# one of the ancestors of u
+				low[u] = min(low[u], low[v])
+
+				# u is an articulation point in following cases
+				# (1) u is root of DFS tree and has two or more children.
+				if parent[u] == -1 and children > 1:
+					ap[u] = True
+
+				#(2) If u is not root and low value of one of its child is more
+				# than discovery value of u.
+				if parent[u] != -1 and low[v] >= disc[u]:
+					ap[u] = True
+					
+				# Update low value of u for parent function calls
+			elif v != parent[u]:
+				low[u] = min(low[u], disc[v])
+
+
+	# The function to do DFS traversal. It uses recursive APUtil()
+	def AP(self):
+
+		# Mark all the vertices as not visited
+		# and Initialize parent and visited,
+		# and ap(articulation point) arrays
+		visited = [False] * (self.V)
+		disc = [float("Inf")] * (self.V)
+		low = [float("Inf")] * (self.V)
+		parent = [-1] * (self.V)
+		ap = [False] * (self.V) # To store articulation points
+
+		# Call the recursive helper function
+		# to find articulation points
+		# in DFS tree rooted with vertex 'i'
+		for i in range(self.V):
+			if visited[i] == False:
+				self.APUtil(i, visited, ap, parent, low, disc)
+
+		for index, value in enumerate (ap):
+			if value == True: print (index,end=" ")
+
+g1 = Graph(5)
+g1.addEdge(1, 0)
+g1.addEdge(0, 2)
+g1.addEdge(2, 1)
+g1.addEdge(0, 3)
+g1.addEdge(3, 4)
+
+print ("\nArticulation points in first graph ")
+g1.AP()
+
+g2 = Graph(4)
+g2.addEdge(0, 1)
+g2.addEdge(1, 2)
+g2.addEdge(2, 3)
+print ("\nArticulation points in second graph ")
+g2.AP()
+
+print("\n") 
+
+
+# Simpler implementation 
+from collections import defaultdict 
+def APUtil(u, graph, visited, ap, parent, low, disc, Time) : 
+    children=0 
+    visited[u]=True 
+    disc[u]=Time[0] 
+    low[u]=Time[0]
+    Time[0]+=1 
+
+    for v in graph[u] : 
+        if not visited[v] : 
+            parent[v]=u 
+            children+=1 
+            APUtil(v, graph, visited, ap, parent, low, disc, Time) 
+            low[u]=min(low[u], low[v])
+            if parent[u]==-1 and children>1 : 
+                ap[u]=True 
+            if parent[u]!=-1 and low[v]>=disc[u] : 
+                ap[u]=True 
+        elif v!=parent[u] : 
+            low[u]=min(low[u], disc[v]) 
+
+def AP(vertices, edges) : 
+    graph=defaultdict(list) 
+    for u, v in edges : 
+        graph[u].append(v) 
+        graph[v].append(u) 
+    visited=[False]*vertices
+    disc=[float("inf")]*vertices
+    low=[float("inf")]*vertices 
+    parent=[-1]*vertices 
+    ap=[False]*vertices
+    Time=[0] 
+
+    for i in range(vertices) : 
+        if not visited[i] : 
+            APUtil(i, graph, visited, ap, parent, low, disc, Time)  
+    for index, value in enumerate(ap) :  
+        if value : 
+            print(index, end=" ")
+print("\nArticulation points in first graph")
+AP(5, [(1, 0), (0, 2), (2, 1), (0, 3), (3, 4)])
+
+print("\nArticulation points in second graph")
+AP(4, [(0, 1), (1, 2), (2, 3)])           
+print("\n")
+
+
+
+## Bridges in Graph 
