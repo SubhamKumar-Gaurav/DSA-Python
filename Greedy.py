fa52451dc95f65ed9340c1ff49c35d1526e04c40
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-03-27 23:32:16 +0530

    Activity Selection Problem

--- Greedy.py
+++ Greedy.py
@@ -21,7 +21,24 @@ def minCoins(coins, amount) :
 coins=[1,2,5,10]
 amount=52
 print("Minimum coins : ", minCoins(coins, amount))
-print("\n")
+print()
+
+
+
+## Activity Selection Problem 
+def activitySelection(arr) : 
+    n=len(arr)
+    arr.sort(key= lambda x: x[1])  
+    prev=0 
+    res=1 
+    for curr in range(1,n) : 
+        if arr[curr][0]>=arr[prev][1] :  
+            res+=1 
+            prev=curr 
+    return res 
+arr=[[1,3], [2,4], [3,8], [10,11]]
+print("Activity selection problem : ",activitySelection(arr))
+print()
 
 
 
