3986677a883ec96d790616da5b3a6fa04bd8155c
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-03-27 23:02:16 +0530

    Min coins

--- Greedy.py
+++ Greedy.py
@@ -4,3 +4,24 @@
 #   Job sequencing 
 #   Huffman Coding 
 #   Huffman Algorithms 
+
+
+## Minimum coins 
+def minCoins(coins, amount) : 
+    coins.sort(reverse=True) 
+    res=0 
+    for x in coins : 
+        if x<=amount : 
+            c=amount//x 
+            res+=c 
+            amount-=(c*x) 
+        if amount==0 : 
+            break 
+    return res 
+coins=[1,2,5,10]
+amount=52
+print("Minimum coins : ", minCoins(coins, amount))
+print("\n")
+
+
+
