b8822d7dc0c8024089753229a3abbcda12164165
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-04-05 00:02:53 +0530

    LCS (recursion, memoization)

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -33,7 +33,7 @@ print("Fibonacci (Recursion) : ",fibo(7))
 # Fibonacci using DP (memoization) 
 memo=[None]*100   # Assumption n<100 
 def fib(n) : 
-    if memo[n]!=None : 
+    if memo[n]!=None :  
         return memo[n] 
     if n==0 or n==1 : 
         memo[n]=n 
@@ -57,3 +57,34 @@ print("\n")
 
 
 ## Longest Common Subsequence 
+# Recursive solution 
+def lcsRec(s1, s2, n, m) : 
+    if n==0 or m==0 : 
+        return 0 
+    if s1[n-1]==s2[m-1] : 
+        return 1+lcsRec(s1, s2, n-1, m-1) 
+    else : 
+        return max(lcsRec(s1, s2, n, m-1), lcsRec(s1, s2, n-1, m)) 
+s1="ABCDGH"
+s2="AEDFHR" 
+print("LCS using Recursion : ",lcsRec(s1, s2, len(s1), len(s2))) 
+
+# Memoization 
+M=1000 
+N=1000 
+memo=[[-1]*(N) for i in range(M)] 
+def lcsMemo(s1, s2, n, m) : 
+    if memo[n][m]!=-1 : 
+        return memo[n][m] 
+    if n==0 or m==0 : 
+        memo[n][m]=0 
+    else : 
+        if s1[n-1]==s2[m-1] : 
+            memo[n][m]=1+lcsMemo(s1, s2, n-1, m-1) 
+        else : 
+            memo[n][m]=max(lcsMemo(s1, s2, n-1, m), lcsMemo(s1, s2, n, m-1)) 
+    return memo[n][m] 
+print("LCS using Memoization : ", lcsMemo(s1, s2, len(s1), len(s2))) 
+print("\n") 
+
+
