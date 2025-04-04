4448d23e2a4b5b6f6878a7aa876ef716fe40606e
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-04-05 00:27:37 +0530

    LCS using Tabulation

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -85,6 +85,22 @@ def lcsMemo(s1, s2, n, m) :
             memo[n][m]=max(lcsMemo(s1, s2, n-1, m), lcsMemo(s1, s2, n, m-1)) 
     return memo[n][m] 
 print("LCS using Memoization : ", lcsMemo(s1, s2, len(s1), len(s2))) 
+
+# Tabulation 
+def lcsTabu(s1, s2) : 
+    m=len(s1)
+    n=len(s2) 
+    dp=[[None]*(n+1) for i in range(m+1)] 
+    for i in range(m+1) : 
+        for j in range(n+1) : 
+            if i==0 or j==0 : 
+                dp[i][j]=0 
+            elif s1[i-1]==s2[j-1] : 
+                dp[i][j]=1+dp[i-1][j-1]
+            else : 
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1])
+    return dp[m][n] 
+print("LCS using Tabulation : ", lcsTabu(s1, s2)) 
 print("\n") 
 
 
