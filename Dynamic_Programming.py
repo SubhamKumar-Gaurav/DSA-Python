1662823a6e7fc3e49d5afd03e7038d1c09f10425
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-05-03 12:13:02 +0530

    0-1 Knapsack

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -404,3 +404,35 @@ print("Min Jumps to reach end (DP) : ", minJumpsDP(arr))
 print("\n") 
 
 
+## 0-1 knapsack problem 
+# Recursive solution   
+def knspSackRec(W, wt, val, n) : 
+    if n==0 or W==0 : 
+        return 0 
+    if wt[n-1]>W : 
+        return knspSackRec(W, wt, val, n-1) 
+    else :  
+        return max(val[n-1]+knspSackRec(W-wt[n-1], wt, val, n-1), knspSackRec(W, wt, val, n-1)) 
+val=[60,100,120]
+wt=[10,20,30] 
+W=50
+print("0-1 Knapsack (Recursive) : ", knspSackRec(W, wt, val, 3)) 
+
+
+# DP solution 
+def knspSackDP(W, wt, val) : 
+    n=len(wt) 
+    dp=[[0 for _ in range(W+1)] for x in range(n+1)]  
+    for i in range(1,n+1) : 
+        for j in range(1, W+1) : 
+            if wt[i-1]<=j : 
+                dp[i][j]=max(dp[i-1][j], dp[i-1][j-wt[i-1]]+val[i-1]) 
+            else : 
+                dp[i][j]=dp[i-1][j] 
+    return dp[n][W] 
+val=[60,100,120]
+wt=[10,20,30] 
+W=50
+print("0-1 Knapsack (DP) : ", knspSackDP(W, wt, val))  
+print("\n")
+
