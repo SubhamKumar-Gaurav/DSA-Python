b0b5a1e84a51aeeaa6e6b80527890383b8bba0f2
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-05-15 18:47:18 +0530

    Egg dropping puzzle

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -474,8 +474,24 @@ def maxValDP(arr) :
             dp[i][j]=max( arr[i]+ min(dp[i+2][j], dp[i+1][j-1]), 
                           arr[j]+ min(dp[i][j-2], dp[i+1][j-1]) ) 
     return dp[0][n-1] 
-# arr=[20,5,4,6,8,3] 
 print("Max val in a game (DP) : ", maxValDP(arr)) 
 print("\n") 
 
 
+## Egg dropping puzzle  
+def eggDrop(f,e) : 
+    dp=[[0 for _ in range(e+1)] for _ in range(f+1)] 
+    for i in range(1,e+1) : 
+        dp[0][i]=0
+        dp[1][i]=1
+    for i in range(2,f+1) : 
+        dp[i][1]=i 
+    for i in range(2,f+1) : 
+        for j in range(2,e+1) :  
+            dp[i][j]=float("inf") 
+            for x in range(1,i+1) : 
+                dp[i][j]=min(dp[i][j], 1+max(dp[x-1][j-1], dp[i-x][j])) 
+    return dp[f][e] 
+print("Egg dropping puzzle (f=10, e=2) : ", eggDrop(10,2))
+print("Egg dropping puzzle (f=10, e=1) : ", eggDrop(10,1))
+print("\n") 
