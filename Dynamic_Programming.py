bda28a98a3515769235c9fe61ff30ee1b00ea612
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-06-10 09:02:42 +0530

    Subset Sum: DP

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -586,3 +586,22 @@ def subsetSumRec(arr, n, s) :
 arr=[10,5,2,3,6] 
 print("Subset Sum Problem")
 print("Recursive: ",subsetSumRec(arr, 5, 8)) 
+
+# DP solution 
+def subsetSumDP(arr, s) : 
+    n=len(arr)
+    dp=[[0 for i in range(s+1)] for j in range(n+1)]
+    for i in range(n+1) : 
+        dp[i][0]=1 
+    for i in range(1,n+1) : 
+        for j in range(1, s+1) : 
+            if j<arr[i-1] : 
+                dp[i][j]=dp[i-1][j] 
+            else : 
+                dp[i][j]=dp[i-1][j]+dp[i-1][j-arr[i-1]] 
+    return dp[n][s]
+arr=[10,5,2,3,6] 
+print("DP solution: ",subsetSumDP(arr, 8))
+print("\n") 
+
+
