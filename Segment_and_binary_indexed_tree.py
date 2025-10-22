edfff36f9a5ba4e407d79b7edf4f7136aa309d21
SubhamKumar-Gaurav <subhamgaurav2001@gmail.com>
2025-10-22 12:52:40 +0530

    getSum(qs, qe)

--- Segment_and_binary_indexed_tree.py
+++ Segment_and_binary_indexed_tree.py
@@ -10,7 +10,7 @@
 
 
 
-# Segment Tree Construction 
+## Segment Tree Construction 
 def buildTree(ss, se, si) : 
     global arr  
     global tree 
@@ -26,3 +26,24 @@ n=len(arr)
 tree=[0]*(4*n)
 buildTree(0,3,0)
 print(tree) 
+print("\n")
+
+
+
+## Range Query in Segment Tree 
+print("Range Query : ") 
+arr=[10,20,30,40]
+tree=[0]*7
+buildTree(0,3,0) 
+print("arr : ", arr)
+
+def getSumRec(qs, qe, ss, se, si) : 
+    if se<qs or ss>qe : 
+        return 0 
+    if qs<=ss and se<=qe : 
+        return tree[si] 
+    mid=(ss+se)//2  
+    return getSumRec(qs, qe, ss, mid, 2*si+1) + getSumRec(qs, qe, mid+1, se, 2*si+2) 
+
+print("getSum(0,2) : ", getSumRec(0, 2, 0, 3, 0)) 
+print("getSum(1,3) : ", getSumRec(1, 3, 0, 3, 0)) 
